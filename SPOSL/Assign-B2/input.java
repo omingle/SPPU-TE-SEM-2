6edad21e8ff16b68cc68978d73f766501f0606f9
Om Ingle <68648288+omingle@users.noreply.github.com>
2021-06-29 09:44:14 +0530

    Add files via upload

---
+++ SPOSL/Assign-B2/input.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class Input
+{
+    public static void main(String args[])
+    {
+        int a = 10, b = 20;
+
+        if(a > b)
+        {
+            System.out.println(a + " is greater");
+        }
+        else
+        {
+            System.out.println(b + " is greater");
+        }
+    }
+}
